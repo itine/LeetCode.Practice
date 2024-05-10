fb381dd801e9f57aec94b23ee5489d1bc0ece96f
Sergey Alekseev <sergey.alekseev@altenar.com>
2024-05-10 11:08:03 +0300

    solution of 1115. Print FooBar Alternately

--- LeetCode.PrintFooBarAlternately_1115/Program.cs
+++ LeetCode.PrintFooBarAlternately_1115/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿static void Main(string[] args)
+{
+    Console.WriteLine("Hello world");
+}
 
-Console.WriteLine("Hello, World!");
+public class FooBar
+{
+    private readonly int n;
+    private readonly Semaphore _semaphore1;
+    private readonly Semaphore _semaphore2;
+
+    public FooBar(int n)
+    {
+        this.n = n;
+        _semaphore1 = new Semaphore(0, 1);
+        _semaphore2 = new Semaphore(0, 1);
+    }
+
+    public void Foo(Action printFoo)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            if (i != 0)
+                _semaphore2.WaitOne();
+            // printFoo() outputs "foo". Do not change or remove this line.
+            printFoo();
+            _semaphore1.Release();
+        }
+    }
+
+    public void Bar(Action printBar)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            _semaphore1.WaitOne();
+            // printBar() outputs "bar". Do not change or remove this line.
+            printBar();
+            _semaphore2.Release();
+        }
+    }
+}
