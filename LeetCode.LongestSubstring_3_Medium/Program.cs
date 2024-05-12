c47ae7f4cf7d956db41daeada6744c05899db722
Sergey Alekseev <sergey.alekseev@altenar.com>
2024-05-12 19:05:51 +0300

    solution of "3. Longest Substring Without Repeating Characters"

---
+++ LeetCode.LongestSubstring_3_Medium/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.WriteLine(Solution.LengthOfLongestSubstring("abcabcbb")); //The answer must be "abc", with the length of 3.
+
+public class Solution
+{
+    private static readonly int S_MAX_LENGTH = 5 * (int)Math.Pow(10, 4);
+
+    public static int LengthOfLongestSubstring(string s)
+    {
+        if (s.Length <= 1)
+            return s.Length;
+        
+        if (s.Length > S_MAX_LENGTH)
+            return 0;
+
+        HashSet<char> result = new HashSet<char>();
+        int maxLength = 0;
+        int leftIndex = 0;
+        int rightIndex = 0;
+
+        while (rightIndex < s.Length)
+        {
+            if (result.Add(s[rightIndex]))
+            {
+                rightIndex++;
+                maxLength = System.Math.Max(maxLength, result.Count);
+            }
+            else
+            {
+                result.Remove(s[leftIndex]);
+                leftIndex++;
+            }
+        }
+
+        return maxLength;
+    }
+}
