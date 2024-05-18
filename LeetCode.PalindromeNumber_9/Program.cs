f1d8361a9ee5223dca7e68f7b44f9d25193aeb40
Sergey Alekseev <sergey.alekseev@altenar.com>
2024-05-18 16:44:07 +0300

    PalindromeNumber_9 solution

---
+++ LeetCode.PalindromeNumber_9/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.WriteLine(SolutionWithoutAllocation.IsPalindrome(10));
+
+public static class SolutionWithoutAllocation
+{
+    public static bool IsPalindrome(int x)
+    {
+        if (x < 0)
+            return false;
+        
+        if (x / 10 == 0)
+            return true;
+
+        int temp = x;
+        int y = temp % 10;
+        
+        while (temp / 10 != 0)
+        {
+            y = (y * 10) + (temp /= 10) % 10;
+        }
+
+        return y == x;
+    }
+}
+
+
+public static class SolutionWithStringAllocation
+{
+    public static bool IsPalindrome(int x)
+    {
+        string stringNumber = x.ToString();
+
+        for (int i = 0; i < stringNumber.Length / 2; i++)
+        {
+            if (stringNumber[i] != stringNumber[stringNumber.Length - 1 - i])
+                return false;
+        }
+
+        return true;
+    }
+}
