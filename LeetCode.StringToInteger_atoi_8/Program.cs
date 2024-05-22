e28c60a0f8dd3d5bae40b2a619f52cb421b4530f
Sergey Alekseev <sergey.alekseev@altenar.com>
2024-05-22 23:21:00 +0300

    AtoI solution

---
+++ LeetCode.StringToInteger_atoi_8/Program.cs
@@ -0,0 +1,44 @@
+﻿Console.WriteLine(Solution.MyAtoi("   -042"));
+
+public static class Solution
+{
+    public static int MyAtoi(string input)
+    {
+        string s = input.Trim();
+        if (s.Length == 0)
+            return 0;
+
+        int i = 0, sign = 1;
+        int? number = null;
+
+        if (s[0] == '-' || s[0] == '+')
+        {
+            if (s[0] == '-')
+                sign *= -1;
+            i++;
+        }
+
+        for (; i < s.Length; i++)
+        {
+            if (!char.IsDigit(s[i]))
+            {
+                break;
+            }
+
+            try
+            {
+                checked
+                {
+                    int currentDigit = s[i] - '0';
+                    number = (number ?? 0) * 10 + currentDigit;
+                }
+            }
+            catch (OverflowException)
+            {
+                return sign == 1 ? int.MaxValue : int.MinValue;
+            }
+        }
+
+        return (number ?? 0) * sign;
+    }
+}
